1ff84dd3ff67c6d3e57b6b7f5ba80c1600466739
nils <48135649+Nilstrieb@users.noreply.github.com>
2023-06-23 10:53:19 +0200

    formatting

--- tic-tac-toe/App.tsx
+++ tic-tac-toe/App.tsx
@@ -1,21 +1,21 @@
-import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View} from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 import Board from "./Board";
 
 export default function App() {
-    return (
-        <View style={styles.container}>
-            <Board/>
-            <StatusBar style="auto"/>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Board />
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
