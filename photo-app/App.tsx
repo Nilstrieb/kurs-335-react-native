f9371bb2e851241c0acd12259eb3c364f5ea8707
nils <48135649+Nilstrieb@users.noreply.github.com>
2023-06-23 15:32:22 +0200

    get media library

--- photo-app/App.tsx
+++ photo-app/App.tsx
@@ -1,22 +1,34 @@
 import { StatusBar } from "expo-status-bar";
-import { ImageSourcePropType, StyleSheet, Text, View } from "react-native";
+import {
+  ImageSourcePropType,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import ImageViewer, {
   IMAGE_HEIGHT,
   IMAGE_WIDTH,
 } from "./components/ImageViewer";
 import Button from "./components/Button";
 import * as ImagePicker from "expo-image-picker";
-import { useState } from "react";
+import * as MediaLibrary from "expo-media-library";
+import { useRef, useState } from "react";
 import CircleButton from "./components/CircleButton";
 import IconButton from "./components/IconButton";
 import EmojiPicker from "./components/EmojiPicker";
 import EmojiList from "./components/EmojiList";
 import EmojiSticker from "./components/EmojiSticker";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { captureRef } from "react-native-view-shot";
 
 const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
+  const imageRef = useRef<any>();
+  const [status, requestPermission] = MediaLibrary.usePermissions({
+    writeOnly: true,
+  });
   const [pickedEmoji, setPickedEmoji] = useState<ImageSourcePropType | null>(
     null
   );
@@ -25,6 +37,10 @@ export default function App() {
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
   // This counter increases every time the emoji sticker should be reset
   const [emojiStickerResetCounter, setEmojiStickerResetCounter] = useState(0);
+
+  if (status === null) {
+    requestPermission();
+  }
 
   const pickImage = async () => {
     const result = await ImagePicker.launchImageLibraryAsync({
@@ -43,23 +59,35 @@ export default function App() {
 
   const onReset = () => setShowAppOptions(false);
   const onAddSticker = () => setIsModalVisible(true);
-  const onSaveImage = async () => {};
+  const onSaveImage = async () => {
+    if (Platform.OS === "web") {
+      alert("saving is not supported on the web");
+    }
+    try {
+      const localUri = await captureRef(imageRef, { height: 440, quality: 1 });
+      await MediaLibrary.saveToLibraryAsync(localUri);
+    } catch (e) {
+      console.error(e);
+    }
+  };
   const onModalClose = () => setIsModalVisible(false);
 
   return (
     <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer
-          placeholderImg={PlaceholderImage}
-          selectedImgUri={selectedImage}
-        />
-        {pickedEmoji !== null ? (
-          <EmojiSticker
-            imageSize={40}
-            stickerSource={pickedEmoji}
-            currentResetCounter={emojiStickerResetCounter}
+        <View ref={imageRef} collapsable={false}>
+          <ImageViewer
+            placeholderImg={PlaceholderImage}
+            selectedImgUri={selectedImage}
           />
-        ) : null}
+          {pickedEmoji !== null ? (
+            <EmojiSticker
+              imageSize={40}
+              stickerSource={pickedEmoji}
+              currentResetCounter={emojiStickerResetCounter}
+            />
+          ) : null}
+        </View>
       </View>
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
