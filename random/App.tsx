2f54c9389edb0ad095a8949c3f3e2f1ced825a99
nils <48135649+Nilstrieb@users.noreply.github.com>
2023-06-29 09:52:32 +0200

    picsum

--- random/App.tsx
+++ random/App.tsx
@@ -1,11 +1,12 @@
 import { StyleSheet, View } from "react-native";
 import PersonSectionList from "./parts/person-list/PersonSectionList";
 import PersonList from "./parts/person-list/PersonList";
+import PicsumList from "./parts/person-list/PicsumList";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <PersonSectionList />
+      <PicsumList />
     </View>
   );
 }
